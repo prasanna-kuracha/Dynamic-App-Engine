cd4da37328bd6ac4101a04d4c71f7417b1c5a0fa
prasanna-CGIT <prasanna@conglomerateit.com>
2026-05-04 12:06:59 +0000

    prepare for render deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ import authRoutes from './routes/authRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
 app.use(cors());
 app.use(express.json());
